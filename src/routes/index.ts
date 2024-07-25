e13d8bd7d870ca5c7c37adcb83e5d9ef83432862
joaocastro <joaocastro0429@gmail.com>
2024-07-25 06:05:32 -0300

    add listagem

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,34 @@
 import express from 'express'
+import { writeFile } from 'fs/promises'
+import { readFile } from 'fs/promises'
 
 const routes=express.Router()
+const dataSource='./data/list.txt'
+routes.post('/contact',async(req,res)=>{
+   const {name}=req.body
+   if(!name || name.length<2){
+     return res.json({error:"O nome precisa ter pelo menos 2 Caracteres"})
+   }
+   // processamento 
 
-routes.get('/',(req,res)=>{
-  return res.json({msg:"bem vindo a API"})
+   let list:string[]=[]
+   try {
+    const data = await readFile(dataSource, 'utf-8'); 
+     list=data.split('\n')
+    
+   } catch (error) {}
+   list.push(name)
+   await writeFile(dataSource,list.join('\n'))
+   return res.status(201).json({contato:name})
+
+   
 })
 
 
 export default routes
+
+
+
+
+
+
